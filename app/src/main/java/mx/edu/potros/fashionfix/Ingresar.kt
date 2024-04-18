c5ad08e093196e52e7b4ddf6ffb4a9e4bf1589b1
LAPTOP-SL2F8D5S\magda <magda-rmzz@hotmail.com>
2024-04-17 17:57:12 -0700

    Implementación de Strings, cambios en el diseño de la página principal e intento de login con base de datos.

--- app/src/main/java/mx/edu/potros/fashionfix/Ingresar.kt
+++ app/src/main/java/mx/edu/potros/fashionfix/Ingresar.kt
@@ -3,18 +3,66 @@ package mx.edu.potros.fashionfix
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import mx.edu.potros.fashionfix.databinding.ActivityMainBinding
+import android.widget.EditText
 
 class Ingresar : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingresar)
 
-        val button: Button = findViewById(R.id.btnIngresar)
+        val etEmail: EditText = findViewById(R.id.etEmail)
+        val etPassword: EditText = findViewById(R.id.etPassword)
 
-        button.setOnClickListener {
-            var intent: Intent = Intent(this, Closet::class.java)
-            startActivity(intent)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.btnIniciar.setOnClickListener{
+            val mEmail = etEmail.text.toString()
+            val mPassword = etPassword.text.toString()
+
+            when{
+                mEmail.isEmpty() || mPassword.isEmpty()->{
+                    Toast.makeText(baseContext,"E-mail o contraseña incorrecta.",
+                        Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                ingresar(mEmail,mPassword)
+                }
+            }
         }
+    }
+
+    private fun ingresar(email:String,password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithEmail:success")
+                    reload()
+                    //val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+    }
+    private fun reload(){
+        val intent = Intent(this, Closet::class.java)
+        this.startActivity(intent)
     }
 }
